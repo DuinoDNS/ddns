93b21e8bf95a56141c1ddd88a41a0b40b580160f
NAP-Software <info@nap.software>
2022-04-30 05:06:00 +0200

    wip

--- main.c
+++ main.c
@@ -1,20 +1,35 @@
 #include <napc.h>
 #include <ddns.h>
 
-ddns__Instance *i;
+#if defined(ARDUINO)
+	ddnsddns__Instance ddns_instance_obj;
+#endif
+
+ddns__Instance *ddns_instance_ptr = NULL;
 
 void napc_app_setup(const char *platform) {
 	NAPC_IGNORE_VALUE(platform);
 
-	i = malloc(sizeof(* i));
+	/*
+	 * Use malloc() on linux to get diagnostics from valgrind
+	 */
+	#if defined(ARDUINO)
+		ddns_instance_ptr = &ddns_instance_obj;
+	#else
+		ddns_instance_ptr = malloc(sizeof(* i));
+	#endif
 
-	ddns_setup(i, "DUINODNS/CFG.TXT", "DUINODNS/RECORDS.TXT");
+	ddns_setup(
+		ddns_instance_ptr,
+		"DUINODNS/CFG.TXT",
+		"DUINODNS/RECORDS.TXT"
+	);
 }
 
 bool napc_app_loop(napc_time uptime) {
 	NAPC_IGNORE_VALUE(uptime);
 
-	ddns_loop(i);
+	ddns_loop(ddns_instance_ptr);
 
 	return true;
 }
